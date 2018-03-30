24a075affc198888badd023728e3b8582b63fd5c
GlennBell <glenn.s.bell@gmail.com>
2018-03-29 22:42:18 -0400

    Commented the code

--- Java/Loop.java
+++ Java/Loop.java
@@ -1,15 +1,16 @@
 // Demonstrate loops
-// For Loop
 
 public class Loop {
     public static void main(String [] args) {
         int i;
 
+        // For Loop
         System.out.println("For Loop: Count up by 2");
         for(i=1; i<=10; i+=2){
             System.out.println(i);
         }
     
+        // While Loop
         System.out.println("While Loop: Count down by 2");
         while( i > 0 ){
             System.out.println(i);
