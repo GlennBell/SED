99a5cee256b6ab7200e0760a935c639a0d078146
GlennBell <glenn.s.bell@gmail.com>
2018-03-29 22:28:08 -0400

    Updated formatting and presentation

--- Java/Assignment.java
+++ Java/Assignment.java
@@ -1,33 +1,36 @@
 public class Assignment {
     public static void main (String[] args){
-        double right = 4.0;
+        double right = 6.0;
         double left = 10.0;
         double answer, a, b;
 
-        System.out.println("left = " + left);
-        System.out.println("right = " + right);
+        System.out.println("A = " + left);
+        System.out.println("B = " + right);
 
         // Assignment
+        System.out.println("\nAssignment");
         answer = left + right;
-        System.out.println("left(" + left +
-                           ") + right(" + right +
+        System.out.println("A(" + left +
+                           ") + B(" + right +
                            ") = " + answer);
 
         // Unary Operators
+        System.out.println("\nUnary Operator");
         a = left;
         b = right;
 
-        System.out.println("left++ = " + a++);
+        System.out.println("A++ = " + a++);
         a = left;
-        System.out.println("++left = " + ++a);
-        System.out.println("right++ = " + b++);
+        System.out.println("++A = " + ++a);
+        System.out.println("B-- = " + b--);
         b = right;
-        System.out.println("++right = " + ++b);
+        System.out.println("--B = " + --b);
 
         // Power function
+        System.out.println("\nPower Function");
         a = left;
         b = right;
 
-        System.out.println("left^right = " + Math.pow(a, b));
+        System.out.println("A^B = " + Math.pow(a, b));
     }
 } 
