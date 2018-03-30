feb927137ef83571f1958e0b02e7247ca4bbe3e0
GlennBell <glenn.s.bell@gmail.com>
2018-03-29 22:03:42 -0400

    Added whitespace for clarity

--- Python/exception.py
+++ Python/exception.py
@@ -10,5 +10,6 @@ numB = float(input(quesB))
 try:
     answer = numA / numB
     print 'Divide ', numA, ' by ', numB, ' = ', answer, '\n'
+
 except ZeroDivisionError:
     print "Very naughty, you can't divide by zero!\n"
