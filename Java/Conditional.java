590a3c4ea6bcb7e1a93c8dfb1f86b69b88f161ca
GlennBell <glenn.s.bell@gmail.com>
2018-03-29 22:39:22 -0400

    Commented the code

--- Java/Conditional.java
+++ Java/Conditional.java
@@ -6,8 +6,10 @@ import java.util.concurrent.ThreadLocalRandom;
 public class Conditional {
     public static void main(String [] args) {
 
+        // Class to read from standard input
         Scanner in = new Scanner(System.in);
     
+        // Generate a random number
         int answer = ThreadLocalRandom.current().nextInt(0, 10);
 
         String question = "What number am I thinking of? ";
@@ -15,9 +17,11 @@ public class Conditional {
         System.out.println("Guess a number between 0 and 9");
     
         while(true){
+            // Ask for input
             System.out.printf(question);
             int guess = in.nextInt();
     
+            // Check to see if the input matches the random number
             if( guess < answer){
                 System.out.println("Too Low");
             } else if( guess > answer ){
