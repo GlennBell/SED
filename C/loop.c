fcf32328f35a255aa9575e370de2e4579a1fdaf2
GlennBell <glenn.s.bell@gmail.com>
2018-04-01 11:44:56 -0400

    Added descriptive comments

--- C/loop.c
+++ C/loop.c
@@ -1,13 +1,22 @@
+/*
+ * Demonstrate the For and While loops
+ *
+ * Compile with: gcc -o loop loop.c
+ *
+ */
+
 #include <stdio.h>
 
 int main(void){
 	int i;
 
+    /* For Loop in C */
     printf("For Loop: Count up by 2\n");
     for(i=1; i<=10; i+=2){
         printf("%d\n", i);
     }
     
+    /* While Loop in C */
     printf("While Loop: Count down by 2\n");
     while( i > 0 ){
 		printf("%d\n", i);
