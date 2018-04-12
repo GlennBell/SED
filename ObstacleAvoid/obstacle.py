ce18bd167f9a899de8d89f1de41e4e9c734c4440
GlennBell <glenn.s.bell@gmail.com>
2018-04-12 10:41:40 -0400

    Cleaned up formatting

--- ObstacleAvoid/obstacle.py
+++ ObstacleAvoid/obstacle.py
@@ -7,17 +7,17 @@ import time
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(3, GPIO.IN)                          #Sensor connection
+GPIO.setup(3, GPIO.IN)  #Sensor connection
 
 try:
-	while True:
-		sensor=GPIO.input(3)                         #Reading output of IR sensor
+    while True:
+        sensor=GPIO.input(3)  #Reading output of IR sensor
 
-		if sensor == 0:                                #IR sensor detects an object
-			print "Obstacle detected ",sensor
-			time.sleep(0.1)
+        if sensor == 0:       #IR sensor detects an object
+            print "Obstacle detected ",sensor
+            time.sleep(0.1)
 
 except KeyboardInterrupt:
-	print "Bye"
-	# Cleanup
-	GPIO.cleanup()
+    print "Bye"
+    # Cleanup
+    GPIO.cleanup()
