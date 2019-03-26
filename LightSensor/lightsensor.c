ccaee78f056d8d3f3d832c9053c1be967fca610e
Glenn Bell <glenn.s.bell@gmail.com>
2019-03-25 21:57:09 -0400

    Simplified example code

--- LightSensor/lightsensor.c
+++ LightSensor/lightsensor.c
@@ -1,89 +1,19 @@
-/*
- Sample code for the BH1750 Light sensor for Raspberry Pi
- Connection:
- VCC-5v (Raspberry pin 1)
- GND-GND(Raspberry pin 6)
- SCL-SCL(Raspberry pin 5)
- SDA-SDA(Raspberry pin 3)
- ADD-NC or GND
- */ 
+/* Source: http://www.raspberry-pi-geek.com/Archive/2017/22/The-BH1750-Digital-Light-Sensor
+ *
+ * Compile: cc -o lightsensor lightsensor.c -lwiringPi
+ */
+#include <wiringPiI2C.h>
 #include <stdio.h>
-#include <stdint.h>
-#include <fcntl.h>
-#include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
-#include <math.h>
-#include <linux/i2c-dev.h>
-#include <linux/i2c.h>
-#include <sys/ioctl.h>
-#include "smbus.h"
 
-#define BH1750FVI_I2C_ADDRESS 0x23 // ADDR > 0.7 VCC)
-//#define BH1750FVI_I2C_ADDRESS  0x53 // ADDR < 0.3 VCC) 
-#define DEBUG 0
-
-#define PowerDown 	0x00
-#define PowerOn 	0x01
-#define Reset		0x07
-#define ContinuHigh	0x10
-#define ContinuLow	0x13
-#define OneTimeHigh	0x20
-#define OneTimeLow	0x23
-
-int main(int argc, char **argv)
-{
-    int fd;
-    char *fileName = "/dev/i2c-1";
-    int retCode;
-    int readSize;
-    unsigned int res;
-    unsigned int lux;
-    char buf[5];
-    int i;
-
-    // Open port for reading and writing
-    if ((fd = open(fileName, O_RDWR)) < 0) {
-        printf("open error\n");
-        exit(1);
+int main (void) {
+    int handle = wiringPiI2CSetup(0x23) ;
+    for( int cntr=0; cntr<10; cntr++){
+        wiringPiI2CWrite(handle,0x10);
+        sleep(1);
+        int word=wiringPiI2CReadReg16(handle,0x00);
+        int lux=((word & 0xff00)>>8) | ((word & 0x00ff)<<8);
+        printf("Current light intensity in Lux:%d \n",lux);
     }
-	
-    // Set the port options and set the address of the device 
-    if (ioctl(fd, I2C_SLAVE, BH1750FVI_I2C_ADDRESS) < 0) {
-        printf("ioctl error\n");
-        close(fd);
-        exit(1);
-    }
-
-    retCode=i2c_smbus_write_byte(fd, PowerOn);
-if(DEBUG)printf("Power On retCode=%d\n",retCode);
-    if (retCode < 0) {
-        printf("PowerOn error\n");
-        close(fd);
-        exit(1);
-    }
-	
-    retCode=i2c_smbus_write_byte(fd, ContinuHigh);
-if(DEBUG)printf("ContinuHigh retCode=%d\n",retCode);
-    if (retCode < 0) {
-        printf("ContinuHigh error\n");
-        close(fd);
-        exit(1);
-    }
-
-    for(i=0;i<10;i++) {
-        sleep(2);
-        readSize = read (fd, buf, 2);
-if(DEBUG)printf("read readSize=%d %x %x\n",readSize,buf[0],buf[1]);
-
-        res = buf[0]*256+buf[1];
-if(DEBUG)printf("res=%x\n",res);
-        lux = res / 1.2;
-        printf("Lux=%d\n",lux);
-    }
-
-    retCode=i2c_smbus_write_byte(fd, PowerDown);
-    close(fd);
-
-    exit (0);
-} 
+    return 0;
+}
